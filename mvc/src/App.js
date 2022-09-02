3fe422837569e8570fedee8fd7cb146556b7305b
Luciano Fastino <lucianofaustino10@gmail.com>
2022-09-02 16:00:16 -0300

    adicionando estrutura inicial

--- mvc/src/App.js
+++ mvc/src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
+import { Header } from './Components/Header'
+import { Footer } from './Components/Footer'
+import { Home } from './Components/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
