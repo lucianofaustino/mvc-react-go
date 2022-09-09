fa745046b58bb4830445f94153127c9cf90200f3
Luciano Fastino <lucianofaustino10@gmail.com>
2022-09-09 09:50:40 -0300

    Foi criado o hook usefetch como função asyncrona para fazer o 'request', mostar o 'error' e mostar o 'loading' no botão.

--- frontend/mvc/src/Components/Login/LoginCadastrar.js
+++ frontend/mvc/src/Components/Login/LoginCadastrar.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import styles from './LoginCadastrar.module.css'
 import { useFormulario } from '../../Hooks/useFormulario'
 import { Button } from '../Formulario/Button'
+import { Error } from '../Ajuda/Error'
 import { Input } from '../Formulario/Input'
 import { USER_POST } from '../../api'
 import { UserContext } from '../../UserContext'
+import { useFetch } from '../../Hooks/useFetch'
 
 
 export const LoginCadastrar = () => {
@@ -13,6 +16,7 @@ export const LoginCadastrar = () => {
   const password = useFormulario()
 
   const { userLogin } = React.useContext(UserContext)
+  const { loading, error, request } = useFetch()
 
   async function handleSubmit(event) {
     event.preventDefault()
@@ -22,19 +26,26 @@ export const LoginCadastrar = () => {
       password: password.value,
     })
 
-    const response = await fetch(url, options)
+    const { response } = await request(url, options)
     if (response.ok) userLogin(username.value, password.value);
   }
 
   return (
-    <section className="animeLeft formulario">
+    <section className={`animeLeft formulario ${styles.cadastro}`}>
       <h1 className="title">Cadastre-se</h1>
       <form onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Email" type="email" name="email" {...email} />
         <Input label="Password" type="password" name="password" {...password} />
-        <Button>Cadastrar</Button>
+
+        {loading ? (
+          <Button disabled>Cadastrando...</Button>
+        ) : (
+          <Button>Cadastrar</Button>
+        )}
+        <Error error={error} />
+
       </form>
-    </section>
+    </section >
   )
 }
