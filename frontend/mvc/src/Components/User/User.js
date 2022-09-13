ef6ca3987f9ef0ad4f7c1ac99a54e50fd3c9706c
Luciano Fastino <lucianofaustino10@gmail.com>
2022-09-13 09:54:47 -0300

    Foi criado um Header para gerenciar as rotas de acesso dentro da /conta de cada usuário. Nele consta a opção de cadastrar um novo ítem, de pesquisar um ítem e um botão de sair que faz o logout. Ainda falta estilizar e criar a versão responsiva.

--- frontend/mvc/src/Components/User/User.js
+++ frontend/mvc/src/Components/User/User.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { UserHeader } from './UserHeader'
+import { Routes, Route } from 'react-router-dom';
+import { UserCadastrarProduto } from './UserCadastroProduto';
+import { UserPesquisarProduto } from './UserPesquisarProduto';
+
 
 export const User = () => {
     return (
-        <div>Usuario</div>
+        <section className="container">
+            <UserHeader />
+            <Routes >
+                <Route path="CadastrarProduto" element={<UserCadastrarProduto />} />
+                <Route path="PesquisarProduto" element={<UserPesquisarProduto />} />
+            </Routes>
+        </section>
     )
 }
---
+++ frontend/mvc/src/Components/User/UserPesquisarProduto.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const UserPesquisarProduto = () => {
+  return (
+    <div>
+      Pesquisar um produto cadastrado
+    </div>
+  )
+}
